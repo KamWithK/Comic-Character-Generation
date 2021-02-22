2def89afc6cb179300f351a28654adbf6bf92d5c
Kamron Bhavnagri <kamwithk@tuta.io>
2021-02-23 00:14:30 +1100

    Fixed image logging

--- src/utils/image_logging.py
+++ src/utils/image_logging.py
@@ -11,14 +11,20 @@ class ImageLoggingCallback(Callback):
         self.num_log = num_log
 
     def on_epoch_end(self, trainer, pl_module):
-        noise = torch.randn(self.num_log, *trainer.datamodule.full_dataset[0][0].shape).to(pl_module.device)
+        noise = torch.randn(self.num_log, *trainer.datamodule.full_dataset[0][0].shape)
+        generations = pl_module(noise.to(pl_module.device))
 
-        pl_module.logger.log_metrics({"Generations": wandb.Image(pl_module(noise))})
+        pl_module.logger.log_metrics({
+            "Generations": [wandb.Image(sample_generation) for sample_generation in generations]
+        })
     
     def on_train_end(self, trainer, pl_module):
         checkpoint = pl_load(trainer.checkpoint_callback.best_model_path)
         pl_module.load_state_dict(checkpoint["state_dict"])
 
-        noise = torch.randn(self.num_log, *trainer.datamodule.full_dataset[0][0].shape).to(pl_module.device)
-        
-        pl_module.logger.log_metrics({"Final Generations": wandb.Image(pl_module(noise))})
+        noise = torch.randn(self.num_log, *trainer.datamodule.full_dataset[0][0].shape)
+        generations = pl_module(noise.to(pl_module.device))
+
+        pl_module.logger.log_metrics({
+            "Generations": [wandb.Image(sample_generation) for sample_generation in generations]
+        })
