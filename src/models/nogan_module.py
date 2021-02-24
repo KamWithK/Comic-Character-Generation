64b37c86e1bc05eb24a93d9410f6cc9ebed53a7d
Kamron Bhavnagri <kamwithk@tuta.io>
2021-02-24 12:29:54 +1100

    Ensured NoGAN trains as expected

--- src/models/nogan_module.py
+++ src/models/nogan_module.py
@@ -17,6 +17,9 @@ class NoGANModule(LightningModule):
         self.pixel_loss = MSELoss()
         self.discriminator_criterion = discriminator_criterion
 
+        # Avoid crash when starting by training the discriminator
+        self.generator_loss = None
+
         # Presetting previous batch to generator, forces first batch to train discriminator
         # Only when pretraining isn't used
         self.pretrain_generator, self.pretrain_discriminator = hparams["pretrain_generator"], hparams["pretrain_discriminator"]
@@ -36,22 +39,13 @@ class NoGANModule(LightningModule):
         return self.generator(input)
 
     def configure_optimizers(self):
-        if self.pretrain_generator == 0 and self.pretrain_discriminator == 0:
-            discriminator_optimizer = Adam(self.discriminator.parameters(), lr=0.0003, betas=(0.9, 0.999))
-        else:
-            discriminator_optimizer = Adam(self.discriminator.parameters(), lr=0.0002, betas=(0.1, 0.999))
-        
-        generator_optimizer = Adam(self.generator.parameters(), lr=0.0002, betas=(0, 0.5))
-        generator_scheduler, discriminator_scheduler = ExponentialLR(generator_optimizer, 0.9), ExponentialLR(discriminator_optimizer, 0.94)
+        generator_optimizer, discriminator_optimizer = Adam(self.generator.parameters()), Adam(self.discriminator.parameters())
 
-        # Force train discriminator first, overridden when not pre-training
-        self.train_stage = "generator"
-
-        return [generator_optimizer, discriminator_optimizer], [generator_scheduler, discriminator_scheduler]
+        return [generator_optimizer, discriminator_optimizer]
 
     # Switch between training generator and discriminator after pretraining
     def on_train_batch_start(self, batch, batch_idx, dataloader_idx):
-        if self.pretrain_generator == 0 and self.pretrain_discriminator == 0:
+        if self.pretrain == False:
             # Update discriminator loss if training the generator
             if self.train_stage == "generator" or "discriminator_loss" not in self.__dict__:
                 self.discriminator_loss = self.hparams["loss_threshold"]
@@ -61,22 +55,16 @@ class NoGANModule(LightningModule):
 
     # Handle pretraining
     def on_epoch_start(self) -> None:
-        if self.pretrain_generator != 0:
+        self.pretrain = True if self.pretrain_generator > 0 or self.pretrain_discriminator > 0 else False
+
+        if self.pretrain_generator > 0:
             self.pretrain_generator -= 1
             self.train_stage = "generator"
-        elif self.pretrain_discriminator != 0:
+        elif self.pretrain_discriminator > 0:
             self.pretrain_discriminator -= 1
             self.train_stage = "discriminator"
-
-        # Reset optimizer to remove momentum after pretraining
-        if self.pretrain_generator == 0 and self.pretrain_discriminator == 0:
-            self.optimizers()[1] = Adam(self.discriminator.parameters(), lr=0.0002, betas=(0.1, 0.999))
             
     def training_step(self, batch, batch_idx, optimizer_idx):
-        # Ensure to only train generator or discriminator, not both in an epoch
-        if optimizer_idx == 0 and self.train_stage != "generator": return
-        if optimizer_idx == 1 and self.train_stage != "discriminator": return
-
         # Get input images and generator output
         imgs, _ = batch
         noise = torch.randn_like(imgs, device=self.device)
@@ -99,16 +87,17 @@ class NoGANModule(LightningModule):
                 "pixel_loss": self.pixel_loss(gen_outputs, imgs),
                 "feature_loss": self.feature_loss(gen_outputs, imgs)
             }
-            self.generator_loss = losses["generator_loss"]
 
             if self.pretrain_generator == 0:
                 losses["adversarial_loss"] = self.discriminator_criterion(self.discriminator(gen_outputs), real)
                 losses["generator_loss"] += losses["adversarial_loss"]
 
-            # Return the total and individual losses
-            self.log_dict(losses, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+            self.generator_loss = losses["generator_loss"]
 
-            self.manual_backward(losses["generator_loss"], optimizer)
+            # Return the total and individual losses
+            self.log_dict(losses, prog_bar=True, logger=True)
+
+            self.manual_backward(self.generator_loss, optimizer)
 
             if accumulated_grad_batches:
                 optimizer.zero_grad()
@@ -120,13 +109,13 @@ class NoGANModule(LightningModule):
                 "fake_loss": self.discriminator_criterion(self.discriminator(gen_outputs.detach()), fake)
             }
 
-            self.discriminator_loss = losses["real_loss"] + losses["fake_loss"]
-            losses["discriminator_loss"] = self.discriminator_loss
+            losses["discriminator_loss"] = losses["real_loss"] + losses["fake_loss"]
+            self.discriminator_loss = losses["discriminator_loss"]
 
             # Return the total and individual losses
-            self.log_dict(losses, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+            self.log_dict(losses, prog_bar=True, logger=True)
 
-            self.manual_backward(losses["discriminator_loss"], optimizer)
+            self.manual_backward(self.discriminator_loss, optimizer)
 
             if accumulated_grad_batches:
                 optimizer.zero_grad()
