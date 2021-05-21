de3a2ac433ef2576b197e7b1969f82dffdf01b8a
Kamron Bhavnagri <kamwithk@tuta.io>
2021-05-21 12:17:38 +1000

    Better training stability with extra features

--- src/models/custom_discriminator.py
+++ src/models/custom_discriminator.py
@@ -5,24 +5,28 @@ from models.self_attention import SelfAttention
 from itertools import starmap
 
 class Discriminator(nn.Module):
-    def __init__(self, in_size=128, channels=3, hidden_dims=[32, 64, 128, 256, 512], block=None):
+    def __init__(self, channels=3, hidden_dims=[32, 64, 128, 256, 512], block=None):
         super().__init__()
         
         # Custom block support        
         if block != None: self.block = block
         
         in_dims = [channels] + hidden_dims[:-1]
-        self.main = nn.Sequential(*starmap(self.block, zip(in_dims, hidden_dims)))
+        self.main = nn.Sequential(
+            # SelfAttention(in_dims[0]),
+            *starmap(self.block, zip(in_dims, hidden_dims))
+        )
         
     def block(self, in_channels, out_channels):
         # Custom final layer values for better performance
-        stride = 2 if out_channels != 1 else 1
-        padding = 1 if out_channels != 1 else 0
-
-        return nn.Sequential(
-            spectral_norm(nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=stride, padding=padding, bias=False)),
-            nn.LeakyReLU(0.2)
-        )
+        if out_channels != 1:
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.LeakyReLU(0.2)
+            )
+        else:
+            return nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=1, padding=0, bias=False)
 
     def forward(self, input):
         return self.main(input)
--- src/models/custom_generator.py
+++ src/models/custom_generator.py
@@ -5,7 +5,7 @@ from models.self_attention import SelfAttention
 from itertools import starmap
 
 class Generator(nn.Module):
-    def __init__(self, in_size=1, out_size=256, latent_dims=128, out_channels=3, hidden_dims=[512, 256, 128, 64, 32], block=None):
+    def __init__(self, latent_dims=128, hidden_dims=[512, 256, 128, 64, 32], block=None):
         super().__init__()
         
         # Custom block support        
@@ -23,11 +23,20 @@ class Generator(nn.Module):
         )
         
     def block(self, in_channels, out_channels):
-        return nn.Sequential(
-            spectral_norm(nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1)),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-        )
+        if in_channels == 128:
+            return nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(),
+            )
+        elif out_channels == 3:
+            return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False)
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(),
+            )
 
     def forward(self, input):
         return self.final_layer(self.main(input))
